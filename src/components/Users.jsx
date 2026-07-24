989e451c59e8546730861534f6fbbab3fee5de5e
shantunu007 <chakmashantunu.web@gmail.com>
2026-07-24 22:26:32 +0600

    Implement add user form submission

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -4,11 +4,30 @@ import { use } from "react";
 const Users = ({ usersPromise }) => {
     const usersData = use(usersPromise);
     console.log(usersData);
+
+    const handleAddUser = e => {
+        e.preventDefault();
+        const form = e.target;
+        const name = form.name.value;
+        const email = form.email.value;
+        const user = { name, email };
+        console.log(user);
+    }
     return (
         <div>
-            {
-                usersData.map((user) => <p key={user.id}>{user.name} : {user.email}</p>)
-            }
+
+            <form onSubmit={handleAddUser}>
+                <input name="name" type="text" />
+                <br />
+                <input name="email" type="email" />
+                <br />
+                <input type="submit" value="add user" />
+            </form>
+            <div>
+                {
+                    usersData.map((user) => <p key={user.id}>{user.name} : {user.email}</p>)
+                }
+            </div>
         </div>
     );
 };
